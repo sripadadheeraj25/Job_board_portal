b083933e30cc4a1c3701cede8c79d5f9ff9c3b94
SRIPADA DHEERAJ <sripadadheeraj2025@gmail.com>
2026-06-03 09:02:58 +0530

    Fix supabase storage URL configuration

--- Job_board/settings.py
+++ Job_board/settings.py
@@ -162,9 +162,9 @@ if not DEBUG:
     AWS_ACCESS_KEY_ID = config('SUPABASE_KEY')
     AWS_SECRET_ACCESS_KEY = config('SUPABASE_KEY')
     AWS_STORAGE_BUCKET_NAME = 'media'
-    AWS_S3_ENDPOINT_URL = config('SUPABASE_URL') + '/storage/v1/s3'
-    AWS_S3_REGION_NAME = config('SUPABASE_REGION')
+    AWS_S3_ENDPOINT_URL = f"https://{config('SUPABASE_PROJECT_ID')}.supabase.co/storage/v1/s3"
+    AWS_S3_REGION_NAME = 'ap-northeast-1'
     AWS_DEFAULT_ACL = 'public-read'
     AWS_QUERYSTRING_AUTH = False
     AWS_S3_FILE_OVERWRITE = False
-    MEDIA_URL = config('SUPABASE_URL') + '/storage/v1/object/public/media/'
+    MEDIA_URL = f"https://{config('SUPABASE_PROJECT_ID')}.supabase.co/storage/v1/object/public/media/"
