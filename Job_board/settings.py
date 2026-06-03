6a557fa1c9f2f1fc862236a984eec80baac97bca
SRIPADA DHEERAJ <sripadadheeraj2025@gmail.com>
2026-06-03 09:16:59 +0530

    Fix S3 access keys for supabase storage

--- Job_board/settings.py
+++ Job_board/settings.py
@@ -159,11 +159,11 @@ CSRF_TRUSTED_ORIGINS = config(
 # Supabase Storage for media files
 if not DEBUG:
     DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-    AWS_ACCESS_KEY_ID = config('SUPABASE_KEY')
-    AWS_SECRET_ACCESS_KEY = config('SUPABASE_KEY')
+    AWS_ACCESS_KEY_ID = config('SUPABASE_S3_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = config('SUPABASE_S3_SECRET_ACCESS_KEY')
     AWS_STORAGE_BUCKET_NAME = 'media'
-    AWS_S3_ENDPOINT_URL = f"https://{config('SUPABASE_PROJECT_ID')}.supabase.co/storage/v1/s3"
-    AWS_S3_REGION_NAME = 'ap-northeast-1'
+    AWS_S3_ENDPOINT_URL = config('SUPABASE_S3_ENDPOINT')
+    AWS_S3_REGION_NAME = config('SUPABASE_REGION')
     AWS_DEFAULT_ACL = 'public-read'
     AWS_QUERYSTRING_AUTH = False
     AWS_S3_FILE_OVERWRITE = False
